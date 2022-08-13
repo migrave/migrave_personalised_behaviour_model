3d15bc135da31a5df14a9395806a5c7a80211d22
Intelwi <mlk04@wp.pl>
2022-08-13 12:15:36 +0200

    Changed structure of behaviour model

---
+++ behaviour_model_training.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+'''
+    Copyright 2022 by Michał Stolarz <michal.stolarz@h-brs.de>
+
+    This file is part of migrave_personalised_behaviour_model.
+
+    migrave_personalised_behaviour_model is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    migrave_personalised_behaviour_model is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+    You should have received a copy of the GNU Affero General Public License
+    along with migrave_personalised_behaviour_model. If not, see <http://www.gnu.org/licenses/>.
+'''
+
+from behaviour_model.BehaviourModel import BehaviourModel
+
+behaviour_model = BehaviourModel()
+behaviour_model.train()
